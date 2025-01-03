5fce6a3446f915f23f2e49ffbdbaf3688a3596fd
Tristan Batchler <info@tbat.me>
2025-01-03 21:27:59 +1000

    Add boot splash configuration and define gold bars item in defaults

--- server/internal/central/items/defaults.go
+++ server/internal/central/items/defaults.go
@@ -4,6 +4,7 @@ import "github.com/tristanbatchler/TwilightGroveOnline/server/internal/objs"
 
 const logsKey = "Logs"
 const rocksKey = "Rocks"
+const goldBarsKey = "GoldBars"
 
 var Defaults = map[string]*objs.Item{
 	logsKey: {
@@ -20,7 +21,15 @@ var Defaults = map[string]*objs.Item{
 		SpriteRegionY: 80,
 		DbId:          0, // 0 will be checked for to signal the actual ID needs to be looked up
 	},
+	goldBarsKey: {
+		Name:          "Golden bars",
+		Description:   "Pure gold formed into perfect ingots and stamped with the royal seal. Offical currency of the realm.",
+		SpriteRegionX: 64,
+		SpriteRegionY: 80,
+		DbId:          0, // 0 will be checked for to signal the actual ID needs to be looked up
+	},
 }
 
 var Logs = Defaults[logsKey]
 var Rocks = Defaults[rocksKey]
+var GoldBars = Defaults[goldBarsKey]
