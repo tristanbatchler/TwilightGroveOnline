3f459acd3c80cf06ff17db7f28077a4d18b27cde
Tristan Batchler <info@tbat.me>
2025-01-11 17:42:28 +1000

    Fix registration character preview for mobile. Had to use control node for some reason

--- client/ui/register_form/register_form.gd
+++ client/ui/register_form/register_form.gd
@@ -1,7 +1,7 @@
 class_name RegisterForm
 extends VBoxContainer
 
-@onready var _sprite: Sprite2D = $ActorPreviewHBoxContainer/PanelContainer/MarginContainer/SubViewportContainer/SubViewport/Sprite2D
+@onready var _sprite: Sprite2D = $CharacterPreview/Sprite2D
 @onready var _username_field: LineEdit = $Username
 @onready var _password_field: LineEdit = $Password
 @onready var _confirm_password_field: LineEdit = $ConfirmPassword
@@ -16,6 +16,7 @@ func _ready() -> void:
 	_username_field.text_submitted.connect(func(_s): _password_field.grab_focus())
 	_password_field.text_submitted.connect(func(_s): _confirm_password_field.grab_focus())
 	_sprite_option_button.item_selected.connect(_on_sprite_option_selected)
+	_sprite_option_button.item_focused.connect(_on_sprite_option_selected)
 	_confirm_button.pressed.connect(_submit_form)
 	_confirm_password_field.text_submitted.connect(func(_s): _submit_form())
 	_cancel_button.pressed.connect(form_canceled.emit)
