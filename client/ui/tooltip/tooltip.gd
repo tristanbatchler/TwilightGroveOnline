7326f671c0ac56a26357534a0a43d8e0d70b051d
Tristan Batchler <info@tbat.me>
2025-01-15 18:27:16 +1000

    Enhance tooltip and dialogue box UI with improved layout and theming

--- client/ui/tooltip/tooltip.gd
+++ client/ui/tooltip/tooltip.gd
@@ -1,1 +1,7 @@
-extends Node
+extends RichTextLabel
+
+func _ready() -> void:
+	var parent := get_parent() as PopupPanel
+	# We know the parent is always going to be a Panel, since this node is only ever instanced as 
+	# part of custom tooltips (see docs)
+	parent.theme = preload("res://resources/theme/ornate-theme.tres")
